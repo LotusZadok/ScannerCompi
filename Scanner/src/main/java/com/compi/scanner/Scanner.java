1aa41481c28b57f589b439da3d6d533531bb7ca0
Zadok <sebasgranadosa@gmail.com>
2024-08-27 20:15:47 -0600

    Java with Maven Project

---
+++ Scanner/src/main/java/com/compi/scanner/Scanner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.compi.scanner;
+
+/**
+ *
+ * @author sebas
+ */
+public class Scanner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
