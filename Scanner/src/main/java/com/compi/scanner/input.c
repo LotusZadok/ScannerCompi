c563b193b090758c4149582f08dc524193a2b449
XoulEater <ncqueescribir@gmail.com>
2024-11-02 18:33:32 -0600

    Refactor parser and grammar: enhance error reporting and update syntax rules

--- Scanner/src/main/java/com/compi/scanner/input.c
+++ Scanner/src/main/java/com/compi/scanner/input.c
@@ -1,7 +1,87 @@
-const int a = 5;
-global int b, c = 10;
+global int g;
+const int c = 6 > 3;
+global char f;
 
-int main(int a) {
-    int b = 10;
-    return 0;
+int funcion1(){  
+
+    while (x){ 
+        x++;
+    }
+
+    switch(x) {            
+        case 5: {break;}  
+        case 8: {break;}  
+        case x: {break;}  
+        default: x++;  
+    }
+
+    read(X);
+    write(C- 123);
+
 }
+
+
+// a = a * funcion(8);
+// a = a * c // error por el punto y coma
+
+// 	if (y>=0) {
+// 		x= a+b; 
+// 	}
+// 	else {
+// 		x--; 
+// 	}
+// 	else { //deberia dar error por doble uso de else
+// 		x= a+b;
+// 	} 
+        
+
+// 	if x==3 {//deberia dar error por expresion fuera de parentesis
+// 		x= a+b;
+// 	} 
+// 	else {
+// 		x--;
+// 	} 
+// }
+
+
+// int funcion4(int, int) { //no se permite solo los tipos
+	
+// 	if ( (x<7) && (x>2) ){
+//  		x= g+6;
+// 	}
+	
+// 	while ( ){ //debe dar error porque falta expresion
+// 		x= a+b; 	
+// 		break;
+// 	}
+	
+// 	for (x=0; ) { //error pues hay solamente una expresion 
+// 		x= a+b; 	
+// 		break;
+// 	}
+// }
+
+// int funcion1( int x   {  // falta parentesis
+
+// 	while (x && y){ 
+// 		x++;
+//  	}
+	
+// 	switch(x) {            
+
+// 	case 5:    //se espera error por no tener ;
+//     case 8: {break;}  
+// 	case 5: {break;}  
+// 	case x: {break;}  //error debe ser constante              
+// 	default 5: {break;} //error por el 5, default no lleva expresion_constante       
+// 	default: x++;  //se espera error por doble default habiendo quitado el error anterior              
+//  	}
+
+// read(X);  
+// read(123);  //se espera error, pues no se acepta expresion entre ( )
+// read(x++); //se espera error
+
+// write(C- 123);
+// write( );     //se espera error
+
+// }  
